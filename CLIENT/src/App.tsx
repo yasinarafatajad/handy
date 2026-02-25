9e59aae0a61b29369df602f7ae599d6e0e82a3cd
yasinarafatajad <yasinarafatazad082@gmail.com>
2026-02-25 12:18:57 +0600

    [Feature]: enable BrowserRouter future flag for upcoming React Router features

--- CLIENT/src/App.tsx
+++ CLIENT/src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter future={{v7_startTransition:true, v7_relativeSplatPath:true}}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/add-task" element={<AddTask />} />
