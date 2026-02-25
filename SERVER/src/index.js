caed051eba4a94a93a1e50750a1e3809635c805f
yasinarafatajad <yasinarafatazad082@gmail.com>
2026-02-25 12:29:29 +0600

    [API]: add CORS policy to backend for cross-origin requests

--- SERVER/src/index.js
+++ SERVER/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import stats from "./routes/Stats.js";
 import taskManagement from "./routes/Task.js";
 
 const port = process.env.PORT || 7000;
@@ -10,12 +11,14 @@ const app = express();
 // conneect to database
 connectDB();
 
+const corsPolicy = {origin: "http://localhost:8080", credentials:true}
 // middleware
-app.use(cors());
+app.use(cors(corsPolicy));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // all end points
+app.use("/api/v1", stats);
 app.use("/api/v1", taskManagement);
 
 // test route
